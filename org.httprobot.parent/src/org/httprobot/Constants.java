904aa8c94dc3370e46d6db605fc756b63e18d158
joan <joan@joan>
2021-11-02 08:11:45 +0100

    org.httrobot.parent first commit

---
+++ org.httprobot.parent/src/org/httprobot/Constants.java
@@ -0,0 +1,9 @@
+package org.httprobot;
+
+public class Constants {
+
+	public static String HTTP_PROTOCOL = "http://";
+	public static String HTTPS_PROTOCOL = "https://";
+	public static String SERVER_URL_KEY = "[@server_url]";
+	public static String START_URL_KEY = "[@start_url]";
+}
